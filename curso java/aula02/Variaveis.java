640672657332d1991e21f12e79e7e7b339f9064a
hikarocarvalho <hikarofcarvalho@gmail.com>
2021-09-20 18:45:01 -0300

    first classes

---
+++ curso java/aula02/Variaveis.java
@@ -0,0 +1,15 @@
+public class Variaveis{
+    public static void main (String[] args){
+        String nome = "hikaro fernandes";
+        int numero = 5;
+        int numero2 = 10;
+
+        int calculo = numero * numero2;
+        //declare one const value
+        final int numero3 = 3;
+
+        System.out.println(calculo);
+        System.out.println(nome);
+        System.out.println(numero3);
+    }
+}
---
+++ curso java/aula03/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis{
+    public static void main (String[] args){
+        // inteiros
+        int numberInt = 10;
+        // float
+        float numberFloat = 5.5F;
+        // String
+        String firstName = "hikaro";
+        //short
+        short numberShort = 3200;
+        //long
+        long numberLong = 1000000000L;
+
+        System.out.println(numberInt+","+numberFloat+","+firstName+","+numberShort+","+numberLong);
+    }
+}
